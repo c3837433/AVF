d72704f9b3463e520afbdc0326a67837a6f3ae1e
Angessmith <angessmith@fullsail.edu>
2013-12-04 15:21:45 -0600

    Added weather api to iOS.

--- avfApp/platforms/ios/www/js/main.js
+++ avfApp/platforms/ios/www/js/main.js
@@ -14,6 +14,8 @@
 		$("#weather").on("pageinit", runWeather);
 		$("#instagram").on("pageinit", runInstagram);
 		$('#getImages').on('click', getImages);	
+		$('#getWeath').on('click', getDetails);
+		$('#reset').on('click', toggleView);
 	}; // end phonegap whenReady
 //});  // end home pageinit
 
@@ -21,15 +23,63 @@
 //Function to call when the weather API is clicked
 var runWeather = function() {
 	console.log("Weather API Page Loaded");
-		
+	$('#reset').closest('.ui-btn').hide();
 }; // end runWeather
+var toggleView = function(){
+	$('#lookup').show();
+    $('#reset').closest('.ui-btn').hide();
+    $('#resultsWea').empty(); 
+};  // End reset toggle function
+
+var getDetails = function(){
+	$('#lookup').hide();
+    $('#reset').closest('.ui-btn').show();
+	var loc = $('#location').val();
+	var weaApi = "http://openweathermap.org/data/2.5/find?q='" + loc + "'&mode=json&units=imperial&APPID=APIKEY&callback=?&APPID=0fcc58f268f4c29a6e524be5dd1e8fd7";
+	$.ajax({
+    	"url":weaApi,
+        "dataType": "jsonp",
+        "success": function(data) {
+        	//console.log(data);
+        	displayData(data);       
+          } // end success
+	}); // end ajax call
+	return false;
+}; // end get details function
+
+var displayData = function(results){
+	//Empty the Listview;
+	$('#resultsWea').empty();
+	var city = results.list[0];
+	console.log(city);
+	// Create a title message
+	var message = "<h4>Current conditions for " + city.name + ", " + city.sys.country + "</h4>";
+	// Prepend message to the top of content
+	$('#resultsWea').prepend(message);
+	var thisObj = {
+		temp : "Current Temperature: "+ city.main.temp + "&degF",
+		clouds : "Conditions: " + city.weather[0].description,
+		wind : "Wind speed: " + city.wind.speed + " mps" 
+	}; // end thisObj object
+	console.log(thisObj);
+	$.each(thisObj, function(index, value){
+		console.log(value);
+		var list = "<li>" + value + "</li>";
+		$('#resultsWea').append(list);
+	});
+	$('#location').val("");	
+	//$('#resultsWea').show();
+	$('#resultsWea').listview('refresh');
+	//$('#reset').show();
+	//refresh the listview			
+}; // end displayData function
 /* 
 Open Weather Map APPID Key
 0fcc58f268f4c29a6e524be5dd1e8fd7
 
 Open Weather Instructions
 Add the following parameter to the GET request: APPID=APIKEY 
-Example: http://openweathermap.org/data/2.3/forecast/city?id=524901&APPID=1111111111
+Example: http://openweathermap.org/data/2.3/forecast/city?/id=524901&APPID=1111111111
 OR add the following line to http header of request to the server: x-api-key:APIKEY
 */
 
