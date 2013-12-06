b2b65cf467866369effcb1557688d62c9a70862b
Angessmith <angessmith@fullsail.edu>
2013-12-05 23:12:32 -0600

    Changed display of dynamic images.

--- avfApp/platforms/ios/www/js/main.js
+++ avfApp/platforms/ios/www/js/main.js
@@ -2,23 +2,16 @@
 // Angela Smith
 // Week 2
 
-
-// Begin Home page functions
-/*$('#home').on('pageinit', function(){
-    //code needed for home page
-    console.log("Home Page Loaded");
-  */  
-	document.addEventListener("deviceready", whenReady, false);
-	// Listen for when the device is ready, and call functions when clicked
-	function whenReady() {		
-		$("#weather").on("pageinit", runWeather);
-		$("#instagram").on("pageinit", runInstagram);
-		$('#getImages').on('click', getImages);	
-		$('#getWeath').on('click', getDetails);
-		$('#reset').on('click', toggleView);
-	}; // end phonegap whenReady
+document.addEventListener("deviceready", whenReady, false);
+// Listen for when the device is ready, and call functions when clicked
+function whenReady() {
+    $("#weather").on("pageinit", runWeather);
+    $("#instagram").on("pageinit", runInstagram);
+    $('#getImages').on('click', getImages);
+    $('#getWeath').on('click', getDetails);
+    $('#reset').on('click', toggleView);
+}; // end phonegap whenReady
 //});  // end home pageinit
-
 
 //Function to call when the weather API is clicked
 var runWeather = function() {
@@ -67,22 +60,9 @@ var displayData = function(results){
 		var list = "<li>" + value + "</li>";
 		$('#resultsWea').append(list);
 	});
-	$('#location').val("");	
-	//$('#resultsWea').show();
-	$('#resultsWea').listview('refresh');
-	//$('#reset').show();
-	//refresh the listview			
+	$('#location').val("");
+	$('#resultsWea').listview('refresh'); //refresh the listview
 }; // end displayData function
-/* 
-Open Weather Map APPID Key
-0fcc58f268f4c29a6e524be5dd1e8fd7
-
-Open Weather Instructions
-Add the following parameter to the GET request: APPID=APIKEY 
-Example: http://openweathermap.org/data/2.3/forecast/city?/id=524901&APPID=1111111111
-OR add the following line to http header of request to the server: x-api-key:APIKEY
-*/
-
 
 //Function to call when the Instagram API is clicked
 var runInstagram = function() {
@@ -100,22 +80,28 @@ var getImages = function(){
 	//Replace client with my data. Without callback=?&amp; will not receive results
 	var api = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=bf7a180389d34095a78d6f44b6660f73";
 	$.getJSON(api, displayImages);
-	return false;
-
+	return false; // stop page from changing
 };
 var displayImages = function(results){
 	//Empty the Listview
-	$('#resultsInst').empty();
+	$('#resultsInst').empty()
+	console.log(results);
 	// Sample HTML
 	//<img src="url" alt="user_fullname"/><h2>username</h2><p>caption<p/><p>filter</p>
 	$.each(results.data, function(index, value){
-		var image = "<li><img src='" + value.images.standard_resolution.url + "' alt='" + value.user.full_name + "'/><h2>" + value.user.username + "</h2><p>Current likes: " + value.likes.count + "</p></li>";
-	$('#resultsInst').append(image);	
-	}); // end loop through retrieved results
+           if (value.location !=null) { // if the location is not empty
+           console.log(value.location);
+           if(value.location.name != null) { // and if the name is not empty use it
+           console.log(value.location.name);
+           var loc = value.location.name;
+           }
+           } else { // otherwise skip it
+		   var loc = "";
+           }
+           if (loc === undefined){ // if the location is still undefined (had geo without a name) set to blank
+           var loc = " ";
+           }
+           var image = "<li><h2>" + value.user.username +"</h2><h3 class='ui-li-aside'>Likes &hearts; " + value.likes.count + "<h3><img src='" + value.images.standard_resolution.url + "' alt='" + value.user.full_name + "'/><p>" + loc + "</p><p>" + value.tags + "</p></li>";
+           $('#resultsInst').append(image);
+           }); // end loop through retrieved results
 };  // end displayImages function
-
-/*
- Instagram API info
-CLIENT ID	2599f607aa5047dc979bee933438ef61
-CLIENT SECRET	ceec11d7fbd343ec8bf5e3c332ca53f4
- */
